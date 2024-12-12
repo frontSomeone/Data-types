23eaa957ec7fc143f3d8df7d8ee1dfd4a9f58b05
Roman <halikov_99@bk.ru>
2024-12-12 18:29:11 +0300

    fixed

--- src/app.ts
+++ src/app.ts
@@ -7,16 +7,16 @@ let arr: number[] = [1, 2, 3];
 
 console.log(num, str, bool, n, und, arr);
 
-let num1: number = 125;
-let str1: string = "Hello World";
-let bool1: boolean = false;
-let n1: null = null;
-let und1: undefined = undefined;
-let arr1: number[] = [1, 2, 3, 4, 5, 6];
+num = 34;
+str = "Hello World";
+bool = false;
+n = null;
+und = undefined;
+arr = [3, 2, 1, 5, 4, 6];
 
-console.log(num1, str1, bool1, n1, und1, arr1);
+console.log(num, str, bool, n, und, arr);
 
-let elements: number[] = [...arr, ...arr1];
+let elements: number[] = [...arr];
 
 elements.push(9);
 console.log(elements);
